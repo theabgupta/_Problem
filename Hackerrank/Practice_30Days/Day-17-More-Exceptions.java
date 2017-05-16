542290f6f1d173f0d16db9cade0ba7a50c9a1a40
theabgupta <abgupta.456@gmail.com>
2017-05-17 00:24:21 +0530

    Update and add Solution

--- Hackerrank/Practice_30Days/Day-17-More-Exceptions.java
+++ Hackerrank/Practice_30Days/Day-17-More-Exceptions.java
@@ -1,6 +1,10 @@
-class Calculator :
-    def power(self, a, b):
-        if a>= 0 and b>=0:
-            return a**b
-        else:
-            return "n and p should be non-negative"
+//Write your code here
+class Calculator{
+    int power(int n, int p)throws Exception{
+        if(n>=0 && p>=0){
+            return (int)Math.pow((double)n,(double)p);
+        }else{
+            throw new Exception("n and p should be non-negative");
+        }
+    } 
+}
